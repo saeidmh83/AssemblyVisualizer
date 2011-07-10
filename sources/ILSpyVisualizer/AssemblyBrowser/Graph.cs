995dbe7b6e4f8e47cb857aada9ef4a88249f49d8
Denis Markelov <manunt7@gmail.com>
2011-07-10 19:24:34 +0300

    Assembly Browser: Moved layout algorithm to the more appropriate place

--- sources/ILSpyVisualizer/AssemblyBrowser/Graph.cs
+++ sources/ILSpyVisualizer/AssemblyBrowser/Graph.cs
@@ -46,4 +46,39 @@ namespace ILSpyVisualizer.AssemblyBrowser
 			}
 		}
 	}
+
+	class TypeGraphLayoutAlgorithm : LinLogLayoutAlgorithm<TypeViewModel, Edge<TypeViewModel>, TypeGraph>
+	{
+		public TypeGraphLayoutAlgorithm(TypeGraph visitedGraph, IDictionary<TypeViewModel, Point> positions,
+									  LinLogLayoutParameters parameters)
+			: base(visitedGraph, positions, parameters)
+		{
+
+		}
+
+		protected override void InternalCompute()
+		{
+			var type = VisitedGraph.Root;
+			var savedLayout = GraphLayoutManager.LoadLayout(type);
+			if (savedLayout == null)
+			{
+				base.InternalCompute();
+				GraphLayoutManager.SaveLayout(type, VertexPositions);
+			}
+			else
+			{
+				LoadSavedLayout(savedLayout);
+			}
+		}
+
+		private void LoadSavedLayout(IDictionary<TypeViewModel, Point> results)
+		{
+			VertexPositions.Clear();
+
+			foreach (var result in results)
+			{
+				VertexPositions.Add(result);
+			}
+		}
+	}
 }
